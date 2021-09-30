5341f040d90b01617c7287e1df1ea8935f485abb
Jun <junchenli423@gmail.com>
2021-09-30 17:27:55 -0400

    Post

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,11 +1,11 @@
 const Comment = require('../models/comment')
 
 exports.find = function(req,res,next){
-    console.log('post:', req.params)
+    console.log('post:', req.params.Post)
     Comment.find().exec((err, comments) => {
         console.log(comments)
     })
-    Comment.find({post: req.params.post}).exec(function(err, comments){
+    Comment.find({post: req.params.Post}).exec(function(err, comments){
         if(err) return next(err)
         console.log(comments)
         res.json(comments)
